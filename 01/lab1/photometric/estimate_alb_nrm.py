13a9f1ec894e5a12b214ee2987c1d4892d9de59f
Gianluca Truda <gianlucatruda@gmail.com>
2020-09-10 13:45:34 +0200

    Added colour support for photometric stereo. Fixed NaN issues.

--- 01/lab1/photometric/estimate_alb_nrm.py
+++ 01/lab1/photometric/estimate_alb_nrm.py
@@ -1,24 +1,30 @@
 import numpy as np
 
 
-def estimate_alb_nrm(image_stack, scriptV, shadow_trick=False):
+def estimate_alb_nrm(image_stack, scriptV, shadow_trick=False, colour=False):
 
     # COMPUTE_SURFACE_GRADIENT compute the gradient of the surface
     # INPUT:
     # image_stack : the images of the desired surface stacked up on the 3rd dimension
     # scriptV : matrix V (in the algorithm) of source and camera information
     # shadow_trick: (true/false) whether or not to use shadow trick in solving linear equations
+    # colour: (true/false) whether the image is in colour (extra dimension for channels)
     # OUTPUT:
     # albedo : the surface albedo
     # normal : the surface normal
 
-    h, w, _ = image_stack.shape
+    # Expand dimensions by 1 if grayscale
+    if not colour:
+        image_stack = np.expand_dims(image_stack, 3)
+        scriptV = np.expand_dims(scriptV, 2)
+
+    h, w, n, c = image_stack.shape
 
     # create arrays for
     # albedo (1 channel)
     # normal (3 channels)
-    albedo = np.zeros([h, w])
-    normal = np.zeros([h, w, 3])
+    albedo = np.zeros([h, w, 3])
+    normal = np.zeros([h, w, 3, 3])
 
     """
     ================
@@ -32,34 +38,52 @@ def estimate_alb_nrm(image_stack, scriptV, shadow_trick=False):
         normal at this point is g / |g|
     """
 
-    # for each point in image_stack array
-    for x in range(h):
-        for y in range(w):
+    # Loop through channels
+    for ch in range(c):
+        # for each point in image_stack array
+        for x in range(h):
+            for y in range(w):
 
-            # stack image values into a vector i
-            i = image_stack[x, y, :]
+                # stack image values for channel into a vector i
+                i = image_stack[x, y, :, ch]
+                # Get scriptV for current channel
+                _scriptV = scriptV[:, :, ch]
 
-            if shadow_trick:
-                # construct diagonal matrix scriptI
-                scripti = np.diag(i)
-                # Multiply left (A) and right (B) sides by scriptI
-                A = np.dot(scripti, scriptV)
-                B = np.dot(scripti, i)
-            else:
-                # Use the standard `i = V . g` formula
-                A = scriptV
-                B = i
+                if shadow_trick:
+                    # construct diagonal matrix scriptI
+                    scripti = np.diag(i)
+                    # Multiply left (A) and right (B) sides by scriptI
+                    A = np.dot(scripti, _scriptV)
+                    B = np.dot(scripti, i)
+                else:
+                    # Use the standard `i = V . g` formula
+                    A = _scriptV
+                    B = i
 
-            # solve scriptI * scriptV * g = scriptI * i to obtain g for this point (usine least squares)
-            g = np.linalg.lstsq(A, B, rcond=None)[0]
+                # solve scriptI * scriptV * g = scriptI * i to obtain g for this point (usine least squares)
+                g = np.linalg.lstsq(A, B, rcond=None)[0]
 
-            # Calculate |g|
-            norm_of_g = np.linalg.norm(g)
+                # Calculate |g|
+                norm_of_g = np.linalg.norm(g)
 
-            # albedo at this point is |g|
-            albedo[x, y] = norm_of_g
-            # surface normal at this point is g / |g|
-            normal[x, y, :] = g / norm_of_g
+                # albedo at this point is |g|
+                albedo[x, y, ch] = norm_of_g
+
+                # surface normal at this point is g / |g|
+                with np.errstate(divide='ignore', invalid='ignore'):
+                    # Ignore divide by zero errors
+                    normal[x, y, :, ch] = np.divide(g, norm_of_g)
+
+    # Take mean of the normal across channels
+    if colour:
+        # Ignore nans if colour. Only undefined if nan for ALL channels.
+        normal = np.nanmean(normal, axis=3)
+    else:
+        normal = np.mean(normal, axis=3)
+
+    # Collapse albedo colour channels if grayscale
+    if not colour:
+        albedo = np.nanmean(albedo, axis=2)
 
     return albedo, normal
 
@@ -68,4 +92,4 @@ if __name__ == '__main__':
     n = 5
     image_stack = np.zeros([10, 10, n])
     scriptV = np.zeros([n, 3])
-    estimate_alb_nrm(image_stack, scriptV, shadow_trick=True)
+    estimate_alb_nrm(image_stack, scriptV, shadow_trick=False)
--- 01/lab1/photometric/photometric_stereo.py
+++ 01/lab1/photometric/photometric_stereo.py
@@ -8,34 +8,51 @@ from construct_surface import construct_surface
 
 print('Part 1: Photometric Stereo\n')
 
-def photometric_stereo(image_dir='./SphereGray5/' ):
+
+def photometric_stereo(image_dir='./SphereGray5/', colour=False):
 
     # obtain many images in a fixed view under different illumination
     print('Loading images...\n')
-    [image_stack, scriptV] = load_syn_images(image_dir)
-    [h, w, n] = image_stack.shape
+    if colour:
+        print('Stacking across colour channels...\n')
+        img_stacks, Vs = [], []
+        for i in range(3):
+            [image_stack, scriptV] = load_syn_images(image_dir, channel=i)
+            img_stacks.append(image_stack)
+            Vs.append(scriptV)
+
+        # Reorder from CV2's BGR to RGB
+        img_stacks = [img_stacks[2], img_stacks[1], img_stacks[0]]
+        image_stack = np.stack(img_stacks, axis=-1)
+        scriptV = np.stack(Vs, axis=-1)
+        [h, w, n, c] = image_stack.shape
+    else:
+        print('Assuming single-channel images (grayscale).\n')
+        [image_stack, scriptV] = load_syn_images(image_dir, channel=0)
+        [h, w, n] = image_stack.shape
+
     print('Finish loading %d images.\n' % n)
 
     # compute the surface gradient from the stack of imgs and light source mat
     print('Computing surface albedo and normal map...\n')
-    [albedo, normals] = estimate_alb_nrm(image_stack, scriptV, shadow_trick=False)
-
+    [albedo, normals] = estimate_alb_nrm(
+        image_stack, scriptV, shadow_trick=False, colour=colour)
 
     # integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
     print('Integrability checking\n')
     [p, q, SE] = check_integrability(normals)
 
-    threshold = 0.005;
+    threshold = 0.005
     print('Number of outliers: %d\n' % np.sum(SE > threshold))
-    SE[SE <= threshold] = float('nan') # for good visualization
+    SE[SE <= threshold] = float('nan')  # for good visualization
 
     # compute the surface height
-    height_map = construct_surface( p, q )
+    height_map = construct_surface(p, q)
 
     # show results
     show_results(albedo, normals, height_map, SE)
 
-## Face
+
 def photometric_stereo_face(image_dir='./yaleB02/'):
     [image_stack, scriptV] = load_face_images(image_dir)
     [h, w, n] = image_stack.shape
@@ -47,16 +64,20 @@ def photometric_stereo_face(image_dir='./yaleB02/'):
     print('Integrability checking')
     p, q, SE = check_integrability(normals)
 
-    threshold = 0.005;
+    threshold = 0.005
     print('Number of outliers: %d\n' % np.sum(SE > threshold))
-    SE[SE <= threshold] = float('nan') # for good visualization
+    SE[SE <= threshold] = float('nan')  # for good visualization
 
     # compute the surface height
-    height_map = construct_surface( p, q )
+    height_map = construct_surface(p, q)
 
     # show results
     show_results(albedo, normals, height_map, SE)
 
+
 if __name__ == '__main__':
-    photometric_stereo('photometrics_images/SphereGray25/')
-    # photometric_stereo_face()
+    photometric_stereo('photometrics_images/SphereGray5/', colour=False)
+    photometric_stereo('photometrics_images/SphereColor/', colour=True)
+    photometric_stereo('photometrics_images/MonkeyGray/', colour=False)
+    photometric_stereo('photometrics_images/MonkeyColor/', colour=True)
+    photometric_stereo_face('photometrics_images/yaleB02')
