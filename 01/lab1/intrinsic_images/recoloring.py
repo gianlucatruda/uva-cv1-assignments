b7e0140beae82cb7781542d077c594aa5cd7f9f6
Serafim Korovin <Sima.korovin@rambler.ru>
2020-09-14 12:20:13 +0200

    Added albedo into RGB conversion

--- 01/lab1/intrinsic_images/recoloring.py
+++ 01/lab1/intrinsic_images/recoloring.py
@@ -6,6 +6,10 @@ ball = plt.imread('ball.png')
 albedo = plt.imread('ball_albedo.png')
 shading = cv2.cvtColor(plt.imread('ball_shading.png'), cv2.COLOR_GRAY2RGB)
 
+x,y,z = np.where(albedo != 0)
+print('Albedo:', albedo[x[0],y[0]])
+print("Albedo in RGB space:", albedo[x[0],y[0]]*255)
+
 # conversion of shading to RGB mapped the values to [0,1], therefore (0,255,0) = (0,1,0)
 albedo[np.where(albedo[:,:,] != (0,0,0))[:-1]] = (0,1.,0)
 
