2030e6bf1a42314fdc210aef25d35accce7f4a29
Serafim Korovin <Sima.korovin@rambler.ru>
2020-09-17 15:52:15 +0200

    Gaussians implemented

--- 02/Image_enhancement/gauss1D.py
+++ 02/Image_enhancement/gauss1D.py
@@ -1,9 +1,16 @@
 import numpy as np
 
-def gauss1D( sigma , kernel_size )
+def gauss(x,sigma):
+    return 1/(sigma * np.sqrt(2*np.pi)) * np.exp(-x**2/(2*sigma**2))
+
+def gauss1D(sigma, kernel_size):
     G = np.zeros((1, kernel_size))
-    if kernel_size % 2 == 0
+    if kernel_size % 2 == 0:
         raise ValueError('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     # solution
-	
-	return G
+    raw = gauss(np.arange(int(-kernel_size / 2), int(kernel_size / 2) + 1), sigma)
+    G = raw / np.sum(raw)
+    return G
+
+if __name__ == '__main__':
+    print(gauss1D(2,5))
--- 02/Image_enhancement/gauss2D.py
+++ 02/Image_enhancement/gauss2D.py
@@ -1,3 +1,11 @@
-def gauss2D( sigma , kernel_size ):
-    ## solution
+import numpy as np
+from gauss1D import gauss1D
+
+def gauss2D(sigma, kernel_size):
+    raw = gauss1D(sigma, kernel_size) * gauss1D(sigma, kernel_size).reshape(-1,1)
+    G = raw / np.sum(raw)
     return G
+
+
+if __name__ == '__main__':
+    print(gauss2D(2, 5))
