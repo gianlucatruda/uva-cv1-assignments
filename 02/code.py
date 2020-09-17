d944b03e66d296f131603b8d53ed2404706467b5
Gianluca Truda <gianlucatruda@gmail.com>
2020-09-17 13:33:38 +0200

    N00bish visuals for Q5

--- 02/code.py
+++ 02/code.py
@@ -43,8 +43,29 @@ def createGabor(lamb, theta, psi, sigma, gamma):
 
 
 if __name__ == "__main__":
-    real, im = createGabor(15, 0, 0, 30, 1)
-    fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(real, cmap="gray")
-    ax[1].imshow(im, cmap="gray")
+    fig, ax = plt.subplots(3, 5)
+
+    # Theta
+    for i, val in enumerate([0, pi/4, 2*pi/2, 3*pi/4, pi]):
+        real, im = createGabor(15, val, 0, 30, 1)
+        ax[0][i].imshow(real)
+        ax[0][i].axis('off')
+
+    # Sigma
+    for i, val in enumerate([3, 10, 30, 100, 300]):
+        real, im = createGabor(15, 0, 0, val, 1)
+        ax[1][i].imshow(real)
+        ax[1][i].axis('off')
+
+    # Gamma
+    for i, val in enumerate([0.1, 0.5, 1.0, 1.5, 10.0]):
+        real, im = createGabor(15, 0, 0, 30, val)
+        ax[2][i].imshow(real)
+        ax[2][i].axis('off')
+
+
+    # ax[0].imshow(real, cmap="gray")
+    # ax[1].imshow(im, cmap="gray")
+
+    plt.tight_layout()
     plt.show()
