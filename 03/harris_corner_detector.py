f062a20307db03cc61d8c48f37b4950ca99dda06
Serafim Korovin <Sima.korovin@rambler.ru>
2020-09-24 17:23:19 +0200

    Rotations

--- 03/harris_corner_detector.py
+++ 03/harris_corner_detector.py
@@ -1,11 +1,12 @@
 from scipy.signal import convolve2d
 import numpy as np
-from scipy.ndimage import gaussian_filter, maximum_filter
+from scipy.ndimage import gaussian_filter, maximum_filter, rotate
 import matplotlib.pyplot as plt
 import cv2
+from copy import copy
 
 
-def calculate_harris(img, sigma=3, max_window=3, thr=0.1):
+def calculate_harris(img, sigma=3, window=3, thr=0.1):
     img2d = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     Ix = convolve2d(img2d, np.array([[-1, 0, 1]]), 'same')  # gradient by x-axis
     Iy = convolve2d(img2d.T, np.array([[-1, 0, 1]]), 'same').T  # gradient by y-axis
@@ -15,11 +16,12 @@ def calculate_harris(img, sigma=3, max_window=3, thr=0.1):
     H = (A * C - B**2) - 0.04*(A + C)**2  # cornerness matrix
 
     # searching for local maxima in H to detect corners
-    r, c = np.where((maximum_filter(H, max_window) == H) & (maximum_filter(H, max_window) >= thr*np.max(H)))
+    r, c = np.where((maximum_filter(H, window) == H) & (maximum_filter(H, window) >= thr*np.max(H)))
     return H, r, c
 
 
-def plot_grad(img, r, c):
+def plot_grad(image, r, c):
+    img = copy(image)
     img2d = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     Ix = convolve2d(img2d, np.array([[-1, 0, 1]]), 'same')  # gradient by x-axis
     Iy = convolve2d(img2d.T, np.array([[-1, 0, 1]]), 'same').T  # gradient by y-axis
@@ -35,11 +37,17 @@ def plot_grad(img, r, c):
 
 
 if __name__=='__main__':
-    #fname = 'person_toy/00000003.jpg'
+    #fname = 'person_toy/00000001.jpg'
     fname = 'pingpong/0000.jpeg'
-    img = np.array(plt.imread(fname))
-    img.setflags(write=1)
-    H,r,c = calculate_harris(img, max_window=4, sigma=3, thr=0.05)
+    image = np.array(plt.imread(fname))
+    H,r,c = calculate_harris(image, window=4, sigma=3, thr=0.05)
     print(f'There are {r.shape[0]} corner points')
-    plot_grad(img, r, c)
-    
+    plot_grad(image, r, c)
+
+    rotated = rotate(image, 45)
+    _, r, c = calculate_harris(rotated, window=4, sigma=3, thr=0.05)
+    plot_grad(rotated, r, c)
+
+    rotated = rotate(image, 90)
+    _, r, c = calculate_harris(rotated, window=4, sigma=3, thr=0.05)
+    plot_grad(rotated, r, c)
