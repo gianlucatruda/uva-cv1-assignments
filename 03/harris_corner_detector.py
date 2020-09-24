0d8865971963f441c782739002f13fb182f87854
Serafim Korovin <Sima.korovin@rambler.ru>
2020-09-24 17:29:44 +0200

    Ordering problems

--- 03/harris_corner_detector.py
+++ 03/harris_corner_detector.py
@@ -16,7 +16,7 @@ def calculate_harris(img, sigma=3, window=3, thr=0.1):
     H = (A * C - B**2) - 0.04*(A + C)**2  # cornerness matrix
 
     # searching for local maxima in H to detect corners
-    r, c = np.where((maximum_filter(H, window) == H) & (maximum_filter(H, window) >= thr*np.max(H)))
+    c, r = np.where((maximum_filter(H, window) == H) & (maximum_filter(H, window) >= thr*np.max(H)))
     return H, r, c
 
 
@@ -30,7 +30,7 @@ def plot_grad(image, r, c):
     plt.subplot(2, 2, 2)
     plt.imshow(Iy, cmap='gray')
     plt.subplot2grid((2,2), (1,0), colspan=2)
-    for point in list(zip(c,r)):
+    for point in list(zip(r,c)):
         img = cv2.circle(img, center=point, radius=10, color=(0,0,255), thickness=-1)
     plt.imshow(img)
     plt.show()
