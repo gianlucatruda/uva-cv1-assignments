f1194408b5a413952b982315485b291074be906c
Gianluca Truda <gianlucatruda@gmail.com>
2020-09-28 16:17:05 +0200

    Feature tracking works. Update scale needs tweaking.

--- 03/tracking.py
+++ 03/tracking.py
@@ -1,25 +1,30 @@
-"""
-1. Implement a simple feature-tracking algorithm by following below steps. Name your script tracking.py.
-
-    (a) Locate feature points on the first frame by using the Harris Corner Detector, that you implemented in section 1.
-
-    (b) Track these points using the Lucas-Kanade algorithm for optical flow estimation, that you implemented in section 2.
-
-2. Prepare a video for each sample image sequences. These videos should visualize the initial feature points and the optical flow. Test your imple - mentation and prepare visualization videos for pingpong and person toy samples.
-"""
 
 from pathlib import Path
+import os
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
+from scipy import signal
 
 from harris_corner_detector import calculate_harris, plot_grad
-from lucas_kanade import lucas_kanade, plot_vector_field
+from lucas_kanade import lucas_kanade, lk_on_window, plot_vector_field
+
+
+def grayscale(img: np.ndarray):
+    # Convert to grayscale by averaging channels (if necessary)
+    if img.ndim == 3:
+        return np.mean(img, axis=2)
+    return img
 
 
 if __name__ == '__main__':
     DIR = 'person_toy'
     # DIR = 'pingpong'
+    OUTDIR = 'output'
+
+    savepath = Path(OUTDIR) / DIR
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
 
     # Read in all images in directory
     frames = []
@@ -31,29 +36,67 @@ if __name__ == '__main__':
     rows, cols = frames[0].shape[0], frames[0].shape[1]
 
     # Find Harris corners in first frame
-    H, r, c = calculate_harris(frames[0], sigma=3, window=3, thr=0.01)
-    # plot_grad(frames[0], r, c)
+    H, c, r = calculate_harris(frames[0], sigma=3, window=3, thr=0.003)
 
-    # Construct feature image
-    features = np.zeros((rows, cols))
+    # Ditch any points on edges of image (common)
+    _c, _r = [], []
     for x, y in zip(c, r):
-        features[x, y] = 1
+        if x > 10 and y > 10:
+            _c.append(x)
+            _r.append(y)
+    c, r = _c, _r
+    print(f"Tracking {len(c)} points")
+
+    # plot_grad(frames[0], c, r)
+
+    # Sanity check about ordering of rows and columns
+    # print('R:', r)
+    # print('C:', c)
 
     # Track Harris corners through subsequent frames
-    for i, f in enumerate(frames[1:5]):
-        print("frame:", i)
+    for i, f in enumerate(frames[:-1]):
+        print(f"Frame: {i}/{len(frames)}", end='\r')
+
+        # Convert to grayscale
+        frame = grayscale(frames[i])
+        next_frame = grayscale(frames[i+1])
 
         # Run LK on features
-        V = lucas_kanade(f, frames[i+1])
-        Vx, Vy = V[..., 0], V[..., 1]
+        _c, _r = [], []
+        img = frames[i].copy()
 
-        import ipdb; ipdb.set_trace()
+        for x, y in zip(c, r):
 
-        # Update features by IxVx + IyVy = −It
-        features = -1 @ (features @ Vx + features @ Vy)
+            # Create windows around each Harris point
+            winx_1, winx_2 = max(0, x-10), min(cols, x+10)
+            winy_1, winy_2 = max(0, y-10), min(rows, y+10)
+            block_1 = frame[winx_1:winx_2, winy_1:winy_2]
+            block_2 = next_frame[winx_1:winx_2, winy_1:winy_2]
 
-        plt.imshow(f)
-        plt.imshow(features)
-        plt.show()
+            # Apply lucas kanade on those windows
+            V = lk_on_window(block_1, block_2)
+            Vx, Vy = V[..., 0], V[..., 1]
+
+            # Update features
+            _x = x + Vx
+            _y = y + Vy
+            _c.append(int(_x))
+            _r.append(int(_y))
+
+            # Visualise Harris points, LK windows, motion vectors
+            img = cv2.rectangle(img, (winx_1, winy_1),
+                                (winx_2, winy_2), (255, 0, 0), 1)
+            img = cv2.arrowedLine(
+                img, (x, y), (int(x + 100*Vx), int(y + 100*Vy)),
+                color=(0, 255, 0), thickness=2)
+            img = cv2.circle(img, center=(x, y), radius=2,
+                             color=(0, 0, 255), thickness=3)
+
+        # Save the updated points
+        c, r = _c, _r
+
+        # Save the composite image
+        plt.imshow(img)
+        plt.savefig(f"{savepath}/{str(i).zfill(5)}.png")
 
     # TODO make video
