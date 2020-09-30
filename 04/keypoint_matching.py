d247e25e33f4ff22b105f64d192debb1defbe9a1
Serafim Korovin <Sima.korovin@rambler.ru>
2020-09-30 17:19:10 +0200

    Keypoint macthing refactoring
    The higher the ratio, the more points you find

--- 04/keypoint_matching.py
+++ 04/keypoint_matching.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-def match_keypoints(im1, im2, show_keypoints=False, show_matches=True):
+def match_keypoints(im1, im2, ratio=0.15, show_keypoints=False, show_matches=True):
     # New API https://stackoverflow.com/questions/18561910/cant-use-surf-sift-in-opencv#32735795
     sift = cv2.xfeatures2d.SIFT_create()
     kp1, des1 = sift.detectAndCompute(im1, None)
@@ -10,12 +10,20 @@ def match_keypoints(im1, im2, show_keypoints=False, show_matches=True):
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
 
+    # Apply ratio test
+    good = []
+    for m, n in matches:
+        if m.distance < ratio * n.distance:
+            good.append([m])
+
+    coords = []
+    for match in good:
+        p1 = kp1[match[0].queryIdx].pt
+        p2 = kp2[match[0].trainIdx].pt
+        coords.append((p1, p2))
+        print(p1, p2)
+
     if show_matches:
-        # Apply ratio test
-        good = []
-        for m, n in matches:
-            if m.distance < 0.2 * n.distance:
-                good.append([m])
         # cv.drawMatchesKnn expects list of lists as matches.
         img3 = cv2.drawMatchesKnn(
             im1, kp1, im2, kp2, good, None,
@@ -36,12 +44,13 @@ def match_keypoints(im1, im2, show_keypoints=False, show_matches=True):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    return matches
+    return coords, kp1, kp2
 
 
 if __name__ == '__main__':
     im1 = cv2.imread('boat1.pgm')
     im2 = cv2.imread('boat2.pgm')
 
-    matches = match_keypoints(im1, im2, show_matches=False)
+    matches, kp1, kp2 = match_keypoints(im1, im2, ratio=0.15, show_matches=True)
+
     print(f"Found {len(matches)} matches")
