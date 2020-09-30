151e8786592f12d5c1fd469acddf8d6b38775a3f
Britt0508 <britt2609@hotmail.com>
2020-09-30 16:53:59 +0200

    Basis for RANSAC, trying to get matches converted to usable data

--- 04/RANSAC.py
+++ 04/RANSAC.py
@@ -7,18 +7,33 @@ no_param = 6
 
 
 def RANSAC(image_1, image_2, P=5, N=5):
-    T = match_keypoints(image_1, image_2, show_matches=False)
-    # print(f"Found {len(T)} matches")
+    matches, kp1, kp2 = match_keypoints(image_1, image_2, show_matches=False)
+    matches_coord = []
+    for match in matches:
+        p1 = kp1[match.queryIdx].pt
+        p2 = kp2[match.trainIdx].pt
+        matches_coord.append([p1, p2])
+
+    print(matches_coord)
+    T = matches
     best_parameters = [0] * no_param
+    highest_no = 0
     for n in range(N):
         sampling = random.choices(T, k=P)
-        dx = np.gradient(sampling[0])
-        dy = np.gradient(sampling[1])
-        vector_b = [dx, dy]
+        # dx = transform(sampling[0])
+        # dy = transform(sampling[1])
+        # vector_b = [dx, dy]
         matrix_A = [[sampling[0], sampling[1], 0, 0, 1, 0], [0, 0, sampling[0], sampling[1], 0, 1]]
         # (m1, m2, m3, m4, t1, t2) by solving the equation Ax = b.
         new_param = np.linalg.inv(matrix_A) * vector_b
-        print(new_param)
+
+        inliers = []
+
+        new_no = len(inliers)
+        if new_no > highest_no:
+            highest_no = new_no
+            best_parameters = new_param
+        print(best_parameters)
 
 
 if __name__ == '__main__':
