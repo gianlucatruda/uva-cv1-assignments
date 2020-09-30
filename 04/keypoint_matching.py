b6f5f180cd53bcc9935e62221290dcfac3b1e8f9
Gianluca Truda <gianlucatruda@gmail.com>
2020-09-30 15:59:06 +0200

    Random subset of matches plotted. Autosaving.

--- 04/keypoint_matching.py
+++ 04/keypoint_matching.py
@@ -1,4 +1,6 @@
 import cv2
+import os
+import random
 
 
 def match_keypoints(im1, im2,
@@ -20,7 +22,8 @@ def match_keypoints(im1, im2,
             if m.distance < 0.2 * n.distance:
                 good.append([m])
 
-        # Apply optional limit
+        # Apply optional limit and take random subset
+        random.shuffle(good)
         good = good[:limit] if limit is not None else good
 
         # cv.drawMatchesKnn expects list of lists as matches.
@@ -28,6 +31,7 @@ def match_keypoints(im1, im2,
             im1, kp1, im2, kp2, good, None,
             flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
         cv2.imshow('matching', img3)
+        cv2.imwrite('figs/matches.png', img3)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -49,6 +53,9 @@ def match_keypoints(im1, im2,
 if __name__ == '__main__':
     im1 = cv2.imread('boat1.pgm')
     im2 = cv2.imread('boat2.pgm')
+
+    if not os.path.exists('figs'):
+        os.makedirs('figs')
 
     matches, _, _ = match_keypoints(im1, im2,
                                     show_matches=True,
