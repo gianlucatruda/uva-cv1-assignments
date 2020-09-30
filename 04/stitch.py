2873d9205b596c2903d45521a737cf97b99931da
Gianluca Truda <gianlucatruda@gmail.com>
2020-09-30 20:23:58 +0200

    Dodgy stitching WIP.

--- 04/stitch.py
+++ 04/stitch.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
+import os
 
 from keypoint_matching import match_keypoints
-from sklearn.linear_model import RANSACRegressor # TODO switch to ours
+from sklearn.linear_model import RANSACRegressor  # TODO switch to ours
 
 
 def stitch_images(im1, im2):
@@ -26,24 +27,57 @@ def stitch_images(im1, im2):
     # Solve for t using RANSAC (TODO switch to our own later)
     t = RANSACRegressor().fit(A, b).estimator_.coef_
 
-    import ipdb; ipdb.set_trace()
-
+    # Build transformation matrix and shift vector
+    m1, m2, t1, m3, m4, t2 = t
+    M = np.array([[m1, m2], [m3, m4]])
+    shift = np.array([t1, t2])
 
 
     # 2. estimate the size of the stitched image by calculating the transformed
     # coordinates of the second image
 
-    new_x, new_y = im2_trans.shape
+    xmax, ymax, _ = im2.shape
+    new_corners = []
+    for p in np.array([[0, 0], [xmax, 0],
+                       [0, ymax], [xmax, ymax]]):
+        p_trans = M @ p.T + shift.T
+        new_corners.append(p_trans)
+    new_corners = np.array(new_corners)
 
-    img = np.zeros((new_x, new_y))
+
+    xmin, xmax = new_corners[:, 0].min(), new_corners[:, 0].max()
+    ymin, ymax = new_corners[:, 1].min(), new_corners[:, 1].max()
+
+
+    # new_x, new_y = round(im1.shape[0] + xmax - xmin) + 1, round(im1.shape[1] + ymax - ymin) + 1
+    new_x, new_y = 1000, 1000
+    img = np.zeros((new_x, new_y, 3))
 
     # 3. combine the left.jpg with the transformed right.jpg in one image
+    for x in range(new_x):
+        for y in range(new_y):
+            if x < im1.shape[0] and y < im1.shape[1]:
+                # img[x, y] = im1[x, y]
+                pass
+
+            xt, yt = M @ [x, y] + shift.T
+            xt, yt = round(xt), round(yt)
+
+            if xt < im2.shape[0] and yt < im2.shape[1]:
+                img[x, y] = im2[xt, yt]
+
 
     return img
 
 
 if __name__ == "__main__":
-    im1 = cv2.imread('boat1.pgm')
-    im2 = cv2.imread('boat2.pgm')
 
-    stitch_images(im1, im2)
+    if not os.path.exists('figs'):
+        os.makedirs('figs')
+
+
+    im1 = cv2.imread('left.jpg')
+    im2 = cv2.imread('right.jpg')
+
+    im = stitch_images(im1, im2)
+    cv2.imwrite('figs/stitched.png', im)
