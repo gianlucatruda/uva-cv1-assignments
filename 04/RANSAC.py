99d3a5aa6698a32d0a5b33a2e90cbfeb3432f9c8
Britt0508 <britt2609@hotmail.com>
2020-10-01 17:26:00 +0200

    RANSAC, update

--- 04/RANSAC.py
+++ 04/RANSAC.py
@@ -2,65 +2,77 @@ from keypoint_matching import match_keypoints
 import cv2
 import random
 import numpy as np
+import scipy
 
 no_param = 6
+no_data_required = 20
 
 
-def RANSAC(image_1, image_2, P=10, N=10, RANSAC_TRESHOLD=3):
+def RANSAC(image_1, image_2, P=10, N=10, RANSAC_TRESHOLD=10):
     coords, kp1, kp2 = match_keypoints(image_1, image_2,
                                     show_keypoints=True,
                                     limit=10,
                                     ratio=0.2)
 
     best_parameters = [0] * no_param
-    highest_no = 0
+    best_fit = None
+
     for n in range(N):
         sampling = random.choices(coords, k=P)
-        A_components = []
-        b_components = []
+        new_param = get_parameters(sampling)
 
-        for (x, y), (xprime, yprime) in sampling:
-            A_components.append(np.array([
-                [x, y, 1, 0, 0, 0],
-                [0, 0, 0, x, y, 1]
-            ]))
-            b_components.append(np.array([xprime, yprime]))
+        also_inliers = []
+        best_error = 100
 
-        A = np.vstack(A_components)
-        b = np.hstack(b_components)
+        # Get other inlier points
+        print(coords)
+        for (x, y), (xprime, yprime) in coords:
+            if ((x, y), (xprime, yprime)) not in sampling:
+                point_error =
+                print(point_error)
+                if point_error < RANSAC_TRESHOLD:
+                    also_inliers.append([(x, y), (xprime, yprime)])
 
-        # Get new param = (m1, m2, m3, m4, t1, t2) by solving the equation Ax = b.
-        new_param = np.linalg.pinv(A) * b
-
-        inliers = []
-        x_list = []
-        y_list = []
-        num = 0
-
-        # find lines to the model for all testing points
-        for i in range(sampling.shape[0]):
-            #Get some shit done here
-            
-
-
-
-        x_inliers = np.array(x_list)
-        y_inliers = np.array(y_list)
-
-        new_no = len(inliers)
-        if new_no > highest_no:
-            highest_no = new_no
-            best_parameters = new_param
+        # Check if model is good. If so, check how good.
+        if len(also_inliers) > no_data_required:
+            new_model = get_parameters(sampling + also_inliers)
+            new_error = 
+            if new_error < best_error:
+                best_fit = new_model
+                best_parameters = new_param
         print(best_parameters)
+        print(best_fit)
+        return best_parameters, best_fit
 
 
-# def fit(A, Y):
-#     A_transpose = A.transpose()
-#     ATA = A_transpose.dot(A)
-#     ATY = A_transpose.dot(Y)
-#     model = (np.linalg.inv(ATA)).dot(
-#         ATY)  ## For a linear eq. AP = Y to solve a least sqaure problem,  P = (inverse(A'A))(A'Y)
-#     return model
+# def get_error(data, model):
+#     A = np.vstack([data[:,i] for i in input_columns]).T
+#     B = np.vstack([data[:,i] for i in output_columns]).T
+#     B_fit = np.dot(A, model)
+#     err_per_point = np.sum((B-B_fit)**2,axis=1) # sum squared error per row
+#     return err_per_point
+
+
+def get_parameters(data):
+    A_components = []
+    b_components = []
+
+    # Get matrix A and vector b
+    for (x, y), (xprime, yprime) in data:
+        A_components.append(np.array([
+            [x, y, 1, 0, 0, 0],
+            [0, 0, 0, x, y, 1]
+        ]))
+        b_components.append(np.array([xprime, yprime]))
+
+    A = np.vstack(A_components)
+    b = np.hstack(b_components)
+
+    # Get new param x = (m1, m2, m3, m4, t1, t2) by solving the equation Ax = b.
+    new_param = np.linalg.pinv(A) * b
+    # = maybemodel
+
+    return new_param
 
 
 if __name__ == '__main__':
