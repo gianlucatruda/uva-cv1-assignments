554e1680dba8603b329f817b27921bf88497e058
Serafim Korovin <Sima.korovin@rambler.ru>
2020-10-06 18:40:14 +0200

    Data reading and preparation fixed

--- final_part1/run.py
+++ final_part1/run.py
@@ -1,21 +1,27 @@
+import os
 import numpy as np
 import cv2
+from glob import glob
 from sklearn.cluster import KMeans
 from sklearn import svm
 
+# airplane: 1, bird: 2, ship: 9, horse: 7, car: 3
+labels = [1, 2, 9, 7, 3]
 
 ## Read in the train files
-with open('stl10_binary/train_X.bin', 'rb') as f:
-    data = f.read()
-    # CV2
-    data = np.fromstring(data, np.uint8).reshape(-1, 96, 96, 3)
-    sift = cv2.SIFT_create()
-    points = np.zeros((128))
-    for image in data:
-        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        _, des = sift.detectAndCompute(gray, None)
-        points = np.vstack([points, des])
-        #cv2.imshow('window', image)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
+dataset = [[cv2.imread(path, cv2.IMREAD_GRAYSCALE) for path in glob(f'{os.path.realpath(".")}/img/{label}/*.png')] for label in labels]
 
+# cv2.imshow('window', dataset[0][0])
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+sift = cv2.SIFT_create()
+points = np.zeros((128))
+
+for label in labels:
+    for path in glob(f'{os.path.realpath(".")}/img/{label}/*.png'):
+        image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        _, des = sift.detectAndCompute(image, None)
+        points = np.vstack([points, des])
+
+points = points[1:]
