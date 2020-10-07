4993b8a2090dfdf5e9620e4ac4ea13cf44ca6f98
Serafim Korovin <Sima.korovin@rambler.ru>
2020-10-07 18:20:46 +0200

    Code runtime fix

--- final_part1/run.py
+++ final_part1/run.py
@@ -3,39 +3,34 @@ import numpy as np
 import cv2
 from glob import glob
 from sklearn.cluster import KMeans
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 from sklearn import svm, datasets
 
 # cluster_sizes = [400, 1000, 4000]
-cluster_sizes = 2
-
-
-def kmeans_cluster(points, K):
-    descriptors = np.array(points[0])
-    for descriptor in points[1:]:
-        descriptors = np.vstack((descriptors, descriptor))
-
-    # kmeans = [KMeans(n_clusters=k).fit(points) for k in K]
-    kmeans = KMeans(n_clusters=K).fit(descriptors)
-    return kmeans
 
 
 if __name__ == "__main__":
     # airplane: 1, bird: 2, ship: 9, horse: 7, car: 3
     labels = [1, 2, 9, 7, 3]
+    cluster_sizes = 5
+    load = 0
 
-    sift = cv2.SIFT_create()
-    points = np.zeros((128))
+    if not load:
+        sift = cv2.SIFT_create()
+        points = np.zeros((128))
+        for label in labels:
+            for path in tqdm(glob(f'{os.path.realpath(".")}/img/{label}/*.png')):
+            # for path in glob(f'{os.path.realpath(".")}/img/test_img/*.png'):
+                image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                _, des = sift.detectAndCompute(image, None)
+                points = np.vstack([points, des])
+        points = points[1:]
+        print("SIFT completed")
+        np.savetxt('points.txt', points)
+    else:
+        points = np.loadtxt('points.txt')
 
-    for label in labels:
-        # for path in glob(f'{os.path.realpath(".")}/img/{label}/*.png'):
-        for path in glob(f'{os.path.realpath(".")}/img/test_img/*.png'):
-            image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-            _, des = sift.detectAndCompute(image, None)
-            points = np.vstack([points, des])
-
-    print("SIFT completed")
-
-    kmeans = kmeans_cluster(points, cluster_sizes)
+    kmeans = KMeans(n_clusters=cluster_sizes).fit(points)
     print("cluster completed")
     print(kmeans)
